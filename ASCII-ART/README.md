b5e1b746bcf867d1b28e30c998534a14a23db703
Rahul Jha <theslytherin@yandex.com>
2022-06-19 14:00:16 +0530

    made more changes lol

---
+++ ASCII-ART/README.md
@@ -0,0 +1,11 @@
+# ASCII ART
+An obscenely simple python script that takes an image (jpeg/png) as output and returns a supercool ascii art.
+# Usage
+On linux, it is 
+python3 check.py --source "path to the image" -height "height of the ascii art" >> "destination".
+
+
+Yes, I am lazy.
+
+# Example
+![alt text](catto.png)
--- ASCII-ART/check.py
+++ ASCII-ART/check.py
@@ -1,19 +1,28 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-
-imgpath = input("Enter path to the image (jpeg/png) > ")
+import argparse
+# imgpath = input("Enter path to the image (jpeg/png) > ")
+parser = argparse.ArgumentParser()
+parser.add_argument("--source",type=str, required=True)
+parser.add_argument("-height", type = int, required=True)
+args = parser.parse_args()
+imgpath = args.source
+h = args.height
 img = cv2.imread(imgpath)
-shape = (64,32)
+aspect_ratio = img.shape[0]/img.shape[1]
+import math
+zz = math.floor(aspect_ratio*h/2)
+shape = (h,zz)
 resized = cv2.resize(img, shape)
 # cv2.imshow("Lol",resized)
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
 gray_image = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
 
-cv2.imshow("Grayscale", gray_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# cv2.imshow("Grayscale", gray_image)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
 
 def gen_ascii(img):
     res = []
